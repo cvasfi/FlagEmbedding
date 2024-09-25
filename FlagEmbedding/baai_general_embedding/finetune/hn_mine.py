4f9615950ac382277c1f6633877fc0ae47dd9e39
yunus emre caliskan <yunus.ec@gmail.com>
2024-09-25 15:50:40 +0200

    Change lens and add batch size as a script parameter for hn minining.

--- FlagEmbedding/baai_general_embedding/finetune/hn_mine.py
+++ FlagEmbedding/baai_general_embedding/finetune/hn_mine.py
@@ -1,8 +1,9 @@
 import argparse
 import json
 import random
-import numpy as np
+
 import faiss
+import numpy as np
 from tqdm import tqdm
 
 from FlagEmbedding import FlagModel
@@ -10,14 +11,23 @@ from FlagEmbedding import FlagModel
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name_or_path', default="BAAI/bge-base-en", type=str)
-    parser.add_argument('--input_file', default=None, type=str)
-    parser.add_argument('--candidate_pool', default=None, type=str)
-    parser.add_argument('--output_file', default=None, type=str)
-    parser.add_argument('--range_for_sampling', default="10-210", type=str, help="range to sample negatives")
-    parser.add_argument('--use_gpu_for_searching', action='store_true', help='use faiss-gpu')
-    parser.add_argument('--negative_number', default=15, type=int, help='the number of negatives')
-    parser.add_argument('--query_instruction_for_retrieval', default="")
+    parser.add_argument("--model_name_or_path", default="BAAI/bge-base-en", type=str)
+    parser.add_argument("--input_file", default=None, type=str)
+    parser.add_argument("--candidate_pool", default=None, type=str)
+    parser.add_argument("--output_file", default=None, type=str)
+    parser.add_argument(
+        "--range_for_sampling",
+        default="10-210",
+        type=str,
+        help="range to sample negatives",
+    )
+    parser.add_argument(
+        "--use_gpu_for_searching", action="store_true", help="use faiss-gpu"
+    )
+    parser.add_argument(
+        "--negative_number", default=15, type=int, help="the number of negatives"
+    )
+    parser.add_argument("--query_instruction_for_retrieval", default="")
 
     return parser.parse_args()
 
@@ -34,14 +44,15 @@ def create_index(embeddings, use_gpu):
     return index
 
 
-def batch_search(index,
-                 query,
-                 topk: int = 200,
-                 batch_size: int = 64):
+def batch_search(index, query, topk: int = 200, batch_size: int = 64):
     all_scores, all_inxs = [], []
-    for start_index in tqdm(range(0, len(query), batch_size), desc="Batches", disable=len(query) < 256):
-        batch_query = query[start_index:start_index + batch_size]
-        batch_scores, batch_inxs = index.search(np.asarray(batch_query, dtype=np.float32), k=topk)
+    for start_index in tqdm(
+        range(0, len(query), batch_size), desc="Batches", disable=len(query) < 256
+    ):
+        batch_query = query[start_index : start_index + batch_size]
+        batch_scores, batch_inxs = index.search(
+            np.asarray(batch_query, dtype=np.float32), k=topk
+        )
         all_scores.extend(batch_scores.tolist())
         all_inxs.extend(batch_inxs.tolist())
     return all_scores, all_inxs
@@ -51,21 +62,30 @@ def get_corpus(candidate_pool):
     corpus = []
     for line in open(candidate_pool):
         line = json.loads(line.strip())
-        corpus.append(line['text'])
+        corpus.append(line["text"])
     return corpus
 
 
-def find_knn_neg(model, input_file, candidate_pool, output_file, sample_range, negative_number, use_gpu):
+def find_knn_neg(
+    model,
+    input_file,
+    candidate_pool,
+    output_file,
+    sample_range,
+    negative_number,
+    use_gpu,
+    batch_size,
+):
     corpus = []
     queries = []
     train_data = []
     for line in open(input_file):
         line = json.loads(line.strip())
         train_data.append(line)
-        corpus.extend(line['pos'])
-        if 'neg' in line:
-            corpus.extend(line['neg'])
-        queries.append(line['query'])
+        corpus.extend(line["pos"])
+        if "neg" in line:
+            corpus.extend(line["neg"])
+        queries.append(line["query"])
 
     if candidate_pool is not None:
         if not isinstance(candidate_pool, list):
@@ -74,49 +94,58 @@ def find_knn_neg(model, input_file, candidate_pool, output_file, sample_range, n
     else:
         corpus = list(set(corpus))
 
-    print(f'inferencing embedding for corpus (number={len(corpus)})--------------')
-    p_vecs = model.encode(corpus, batch_size=256)
-    print(f'inferencing embedding for queries (number={len(queries)})--------------')
-    q_vecs = model.encode_queries(queries, batch_size=256)
+    print(f"inferencing embedding for corpus (number={len(corpus)})--------------")
+    p_vecs = model.encode(corpus, batch_size=batch_size, max_length=2100)
+    print(f"inferencing embedding for queries (number={len(queries)})--------------")
+    q_vecs = model.encode_queries(queries, batch_size=batch_size, max_length=400)
 
-    print('create index and search------------------')
+    print("create index and search------------------")
     index = create_index(p_vecs, use_gpu=use_gpu)
     _, all_inxs = batch_search(index, q_vecs, topk=sample_range[-1])
     assert len(all_inxs) == len(train_data)
 
     for i, data in enumerate(train_data):
-        query = data['query']
-        inxs = all_inxs[i][sample_range[0]:sample_range[1]]
+        query = data["query"]
+        inxs = all_inxs[i][sample_range[0] : sample_range[1]]
         filtered_inx = []
         for inx in inxs:
-            if inx == -1: break
-            if corpus[inx] not in data['pos'] and corpus[inx] != query:
+            if inx == -1:
+                break
+            if corpus[inx] not in data["pos"] and corpus[inx] != query:
                 filtered_inx.append(inx)
 
         if len(filtered_inx) > negative_number:
             filtered_inx = random.sample(filtered_inx, negative_number)
-        data['neg'] = [corpus[inx] for inx in filtered_inx]
+        data["neg"] = [corpus[inx] for inx in filtered_inx]
 
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         for data in train_data:
-            if len(data['neg']) < negative_number:
-                samples = random.sample(corpus, negative_number - len(data['neg']) + len(data['pos']))
-                samples = [sent for sent in samples if sent not in data['pos']]
-                data['neg'].extend(samples[: negative_number - len(data['neg'])])
-            f.write(json.dumps(data, ensure_ascii=False) + '\n')
+            if len(data["neg"]) < negative_number:
+                samples = random.sample(
+                    corpus, negative_number - len(data["neg"]) + len(data["pos"])
+                )
+                samples = [sent for sent in samples if sent not in data["pos"]]
+                data["neg"].extend(samples[: negative_number - len(data["neg"])])
+            f.write(json.dumps(data, ensure_ascii=False) + "\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args()
-    sample_range = args.range_for_sampling.split('-')
+    sample_range = args.range_for_sampling.split("-")
     sample_range = [int(x) for x in sample_range]
 
-    model = FlagModel(args.model_name_or_path, query_instruction_for_retrieval=args.query_instruction_for_retrieval)
+    model = FlagModel(
+        args.model_name_or_path,
+        query_instruction_for_retrieval=args.query_instruction_for_retrieval,
+    )
 
-    find_knn_neg(model,
-                 input_file=args.input_file,
-                 candidate_pool=args.candidate_pool,
-                 output_file=args.output_file,
-                 sample_range=sample_range,
-                 negative_number=args.negative_number,
-                 use_gpu=args.use_gpu_for_searching)
+    find_knn_neg(
+        model,
+        input_file=args.input_file,
+        candidate_pool=args.candidate_pool,
+        output_file=args.output_file,
+        sample_range=sample_range,
+        negative_number=args.negative_number,
+        use_gpu=args.use_gpu_for_searching,
+        batch_size=args.batch_size,
+    )
