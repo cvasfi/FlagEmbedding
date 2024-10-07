8a6ceec533f069807fab9d52d86786c92dcbb113
Yunus Emre Caliskan <yunus.ec@gmail.com>
2024-10-07 16:44:34 +0200

    inject try

--- FlagEmbedding/baai_general_embedding/finetune/run.py
+++ FlagEmbedding/baai_general_embedding/finetune/run.py
@@ -4,7 +4,8 @@ import random
 from pathlib import Path
 
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
-from peft.tuners.tuners_utils import BaseTuner
+from peft.mapping import inject_adapter_in_model
+from peft.tuners.tuners_utils import BaseTunerLayer
 from transformers import AutoConfig, AutoTokenizer, HfArgumentParser, set_seed
 
 from .arguments import DataArguments, LoRAArguments, ModelArguments
@@ -135,13 +136,15 @@ def main():
         )
         logger.info("LoRA config: %s", lora_config)
         print(model.model.modules_to_save)
-        # model.model.add_adapter("embeddings", new_lora_config)
+        model.model = inject_adapter_in_model(
+            new_lora_config, model.model, "embeddings"
+        )
         # Step 3: Use BasicTuner to merge adapters
-        tuner = BaseTuner(model, {"embeddings": new_lora_config})
-        tuner.merge_adapter(["embeddings", "default"])
+        # tuner = BaseTunerLayer(model, {"embeddings": new_lora_config})
+        # tuner.merge_adapter(["embeddings", "default"])
 
         #        print(model.model.active_adapters)
-        model.model.print_trainable_parameters()
+        # model.model.print_trainable_parameters()
         print_trainable_parameters(model.model)
 
     if training_args.fix_position_embedding:
